9b5e0743e2cc0b756be89cee91614e5ccaafa5b6
Alejandro Duque C <ducode@outlook.com>
2019-04-14 00:43:12 -0300

    initial project

---
+++ letras/urls.py
@@ -0,0 +1,13 @@
+"""Letras URLs."""
+
+# Django
+from django.urls import path
+from django.views.generic import TemplateView
+
+# View
+from letras import views
+
+
+urlpatterns = [
+
+]
---
+++ letrasmedio/urls.py
@@ -0,0 +1,12 @@
+"""Urls letrasmedio"""
+
+#Django
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path(r'', include('letras.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
